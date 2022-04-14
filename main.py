feb813b2fe78e13b9f20a2f61633d3d54abae729
barkathbasha <barkathbasha423@gmail.com>
2022-04-14 17:09:23 +0100

    Added links to dataframe

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ import pandas as pd
 
 job_name = input(r"Enter your job title to search: ").replace(' ', '-')
 job_name = job_name.lower().replace(' ', '-')
-baseURL = f'https://www.reed.co.uk/jobs/{job_name}-jobs'
+baseURL = 'https://www.reed.co.uk'
+jobs_baseURL = f'{baseURL}/jobs/{job_name}-jobs'
 
-response = requests.get(baseURL)
+response = requests.get(jobs_baseURL)
 # print(response)
 
 soup = BeautifulSoup(response.text, "html.parser")
 job_cards = soup.find_all("article", class_=re.compile('job-result'))
 print(f"Total jobs are {len(job_cards)}")
 columns = ['Title', 'Posted-On', 'Company', 'Salary', 'Min Salary', 'Max Salary', 'Salary based on', 'Location',
-           'Remote/Office', 'Type', 'Time']
+           'Remote/Office', 'Type', 'Time', 'More Info']
 job_df = pd.DataFrame(data=None, columns=columns)
 print(job_df)
 for i, job_card in enumerate(job_cards):
@@ -35,10 +36,10 @@ for i, job_card in enumerate(job_cards):
     job_location_text = job_location_text.split('\n')[0].strip()
     job_remote_text = job_remote if job_remote is None else job_remote.get_text().strip()
     job_time_text = job_time if job_time is None else job_time.get_text().strip()
-    job_type_text = job_time_text.split(',')[0].strip()
-    job_time_text = job_time_text.split(',')[1].strip()
+    job_type_text = job_time_text if job_time_text is None else job_time_text.split(',')[0].strip()
+    job_time_text = job_time_text if job_time_text is None else job_time_text.split(',')[1].strip()
 
-    job_moreinfo_link = job_title['href']
+    job_moreinfo_link = baseURL + job_title['href'] if job_title['href'].startswith('/') else job_title['href']
 
     if 'per day' in job_salary_text:
         min_salary = job_salary_text.split('-')[0].strip()
@@ -60,7 +61,7 @@ for i, job_card in enumerate(job_cards):
         salary_based_on = None
 
     list_df = [job_title_text, job_posted_by_text, job_company_text, job_salary_text, min_salary, max_salary,
-               salary_based_on, job_location_text, job_remote_text, job_type_text, job_time_text]
+               salary_based_on, job_location_text, job_remote_text, job_type_text, job_time_text, job_moreinfo_link]
     # temp_df = pd.DataFrame([list_df], columns=columns)
     # job_df = job_df.append(temp_df, ignore_index=True)
 
