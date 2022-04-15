4720600d79eadf7715be4eb8c5e3243fb735c624
barkathbasha <barkathbasha423@gmail.com>
2022-04-15 16:13:10 +0100

    Different types of Pie charts

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import re
+import time
 
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.title('Dream job Finder')
 
-job_name = st.text_input('Enter your dream job', value='data analyst')
+job_name = st.text_input('Enter your dream job', placeholder='for ex data analyst')
 job_name = job_name.lower().replace(' ', '-')
 baseURL = 'https://www.reed.co.uk'
 jobs_baseURL = f'{baseURL}/jobs/{job_name}-jobs'
@@ -27,10 +30,10 @@ print(job_df)
 def get_min_max_salary(salary_text, term):
     if len(salary_text.split('-')) == 2:
         minsalary = salary_text.split('-')[0].strip()
-        maxsalary = salary_text.split('-')[1].split('per annum')[0].strip()
+        maxsalary = salary_text.split('-')[1].split(term)[0].strip()
     else:
-        minsalary = salary_text.split('per annum')[0].strip()
-        maxsalary = salary_text.split('per annum')[0].strip()
+        minsalary = salary_text.split(term)[0].strip()
+        maxsalary = salary_text.split(term)[0].strip()
     return minsalary, maxsalary
 
 
@@ -71,7 +74,6 @@ for i, job_card in enumerate(job_cards):
         max_salary = None
         salary_based_on = None
 
-
     list_df = [job_title_text, job_posted_by_text, job_company_text, job_salary_text, min_salary, max_salary,
                salary_based_on, job_location_text, job_remote_text, job_type_text, job_time_text, job_moreinfo_link]
     # temp_df = pd.DataFrame([list_df], columns=columns)
@@ -79,5 +81,48 @@ for i, job_card in enumerate(job_cards):
 
     job_df.loc[i, :] = list_df
 
-print(job_df.head())
-st.write(job_df.shape)
+# print(job_df.head())
+
+
+job_df_copy = job_df.copy()
+job_df_copy['Remote/Office'] = job_df_copy['Remote/Office'].fillna('Work from Office')
+
+# st.dataframe(job_df_copy)
+# number of columns having null values
+# st.write(job_df_copy.isna().sum())
+job_df_copy.dropna(inplace=True)
+job_df_copy.drop(columns=['Posted-On', 'Salary'], inplace=True)
+
+job_df_copy['Min Salary'] = job_df_copy['Min Salary'].apply(lambda x: x[1:].replace(',', '')).astype(float)
+
+job_df_copy['Max Salary'] = job_df_copy['Max Salary'].apply(lambda x: x[1:].replace(',', '')).astype(float)
+
+st.write(job_df_copy['Min Salary'].mean(), job_df_copy['Max Salary'].mean())
+st.metric(label='Total Jobs', value=job_df_copy.shape[0])
+# st.dataframe(job_df_copy['Title','Type'])
+# job_df_copy['Type','Title']
+#
+# fig = px.bar(job_df_copy, values='Title')
+fig = go.Figure(data=[go.Pie(labels=job_df_copy['Type'].unique(),
+                             values=job_df_copy['Type'].value_counts(), title='Jobs Grouped by Type')])
+
+st.plotly_chart(fig, use_container_width=True)
+fig = go.Figure(data=[go.Pie(labels=job_df_copy['Remote/Office'].unique(),
+                             values=job_df_copy['Remote/Office'].value_counts(),
+                             title='Jobs Grouped by Remote/Office')])
+
+st.plotly_chart(fig, use_container_width=True)
+fig = go.Figure(data=[go.Pie(labels=job_df_copy['Salary based on'].unique(),
+                             values=job_df_copy['Salary based on'].value_counts(),
+                             title='Jobs Grouped by Salary type')])
+
+st.plotly_chart(fig, use_container_width=True)
+fig = go.Figure(data=[go.Pie(labels=job_df_copy['Location'].unique(),
+                             values=job_df_copy['Location'].value_counts(), title='Jobs Grouped by Location')])
+
+st.plotly_chart(fig, use_container_width=True)
+fig = go.Figure(data=[go.Pie(labels=job_df_copy['Company'].unique(),
+                             values=job_df_copy['Company'].value_counts(), title='Jobs Grouped by Company')])
+
+st.plotly_chart(fig, use_container_width=True)
+st.dataframe(job_df_copy['Company'].value_counts())
